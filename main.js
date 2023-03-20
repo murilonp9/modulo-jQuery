40e35b4679aae25ca3f359928e44c57fb4409f53
Murilo Nogueira <murilo_np96@hotmail.com>
2023-03-20 14:07:01 -0300

    Correção de erros

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
         const itemDigitado = $('#tarefa-digitada').val();
         const novoItem = $('<li></li>').text(itemDigitado);
 
+        $(document). on('submit',function() { 
+            $("input"). val(""); $("textarea")
+            })
+
         novoItem.text(itemDigitado).appendTo('ul');
 
         novoItem.click(function() {
         $(this).css('text-decoration', 'line-through');
 
-        $(document). on('submit',function() { 
-            $("input"). val(""); $("textarea")
-            })
         });
     });
 })
